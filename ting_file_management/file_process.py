e6aab5bc8ed36429722e11e210ac9999978d3d13
ElisaBorba <elisaborbaa@gmail.com>
2024-03-14 11:08:58 -0300

    req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance=Queue()):
+    for item in instance._data:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+
+    list_text = txt_importer(path_file)
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_text),
+        "linhas_do_arquivo": list_text,
+    }
+
+    instance.enqueue(new_dict)
+    print(new_dict)
 
 
 def remove(instance):
@@ -8,3 +25,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+instance = Queue()
